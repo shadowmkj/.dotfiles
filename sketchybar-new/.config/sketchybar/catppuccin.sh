cc37ddcf28053cb37a575f06af7b7b8dcf3588dc
IamShaDoW666 <www.gaming2018@gmail.com>
2025-10-07 02:36:32 +0530

    Add theme configs

---
+++ sketchybar-new/.config/sketchybar/catppuccin.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# --- Catppuccin Mocha Color Palette ---
+export ROSEWATER=0xfff5e0dc
+export FLAMINGO=0xfff2cdcd
+export PINK=0xfff5c2e7
+export MAUVE=0xffcba6f7
+export RED=0xfff38ba8
+export MAROON=0xffeba0ac
+export PEACH=0xfffab387
+export YELLOW=0xfff9e2af
+export GREEN=0xffa6e3a1
+export TEAL=0xff94e2d5
+export SKY=0xff89dceb
+export SAPPHIRE=0xff74c7ec
+export BLUE=0xff89b4fa
+export LAVENDER=0xffb4befe
+
+export TEXT=0xffcdd6f4
+export SUBTEXT1=0xffbac2de
+export SUBTEXT0=0xffa6adc8
+export OVERLAY2=0xff9399b2
+export OVERLAY1=0xff7f849c
+export OVERLAY0=0xff6c7086
+export SURFACE2=0xff585b70
+export SURFACE1=0xff45475a
+export SURFACE0=0xff313244
+export BASE=0xff1e1e2e
+export MANTLE=0xff181825
+export CRUST=0xff11111b
+
+export BLACK=$BASE
+export WHITE=$TEXT
+export GREY=$SUBTEXT0
+export ORANGE=$PEACH
+export MAGENTA=$MAUVE
+
+export TRANSPARENT=0x00000000
+
+# --- General bar colors ---
+export BAR_COLOR=0xa011111b        # Crust with 63% opacity
+export ICON_COLOR=$TEXT            # Color of all icons
+export LABEL_COLOR=$TEXT           # Color of all labels
+export BACKGROUND_1=0x90313244     # Surface0 with 56% opacity
+export BACKGROUND_2=0x9045475a     # Surface1 with 56% opacity
+export FRONT_BG=$GREEN
+export FRONT_ICON=$CRUST
+export BATTERY_1=$GREEN
+export BATTERY_2=$YELLOW
+export BATTERY_3=$PEACH
+export BATTERY_4=$MAROON
+export BATTERY_5=$RED
+
+export POPUP_BACKGROUND_COLOR=$BASE
+export POPUP_BORDER_COLOR=$TEXT
+
+export SHADOW_COLOR=$BLACK
+
+# # Color Palette
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+#
+# # General bar colors
+# export BAR_COLOR=0xa024273a
+# export ICON_COLOR=$WHITE  # Color of all icons
+# export LABEL_COLOR=$WHITE # Color of all labels
+# export BACKGROUND_1=0x9924273a 
+# export BACKGROUND_2=0x90494d64
+#
+# export POPUP_BACKGROUND_COLOR=0xff24273a
+# export POPUP_BORDER_COLOR=$WHITE
+#
+# export SHADOW_COLOR=$BLACK
---
+++ sketchybar-new/.config/sketchybar/gruvbox.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Color Palette (Gruvbox Dark)
+export BLACK=0xff282828      # bg
+export WHITE=0xffebdbb2      # fg
+export RED=0xffcc241d
+export GREEN=0xff98971a
+export BLUE=0xff458588
+export AQUA=0xff8ec07c
+export YELLOW=0xffd79921
+export ORANGE=0xffd65d0e
+export ORANGE_LIGHT=0xfffe8019
+export MAGENTA=0xffb16286    # purple
+export GREY=0xffa89984       # gray
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xa01d2021        # bg0_h with ~63% opacity
+export ICON_COLOR=$WHITE           # Color of all icons
+export LABEL_COLOR=$WHITE          # Color of all labels
+export BACKGROUND_1=0x903c3836     # bg1 with ~56% opacity
+export BACKGROUND_2=0x90504945     # bg2 with ~56% opacity
+export FRONT_BG=$BLACK
+export FRONT_ICON=$GREY
+export BATTERY_1=$GREEN
+export BATTERY_2=$YELLOW
+export BATTERY_3=$ORANGE_LIGHT
+export BATTERY_4=$ORANGE
+export BATTERY_5=$RED
+export POPUP_BACKGROUND_COLOR=0xff282828 # bg
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
